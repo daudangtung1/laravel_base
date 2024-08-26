060e487b29854a7377c197563902bacd1865405c
daudangtung1 <dangtung.dau.bka@gmail.com>
2024-08-26 13:42:14 +0700

    feature/art: art + category

---
+++ Modules/Art/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module Art</title>
+
+       {{-- Laravel Mix - CSS File --}}
+       {{-- <link rel="stylesheet" href="{{ mix('css/art.css') }}"> --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Mix - JS File --}}
+        {{-- <script src="{{ mix('js/art.js') }}"></script> --}}
+    </body>
+</html>
--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,8 +1,33 @@
 <?php
 
-if (!function_exists('demo')) {
-    function demo()
+use Illuminate\Support\Carbon;
+use Intervention\Image\Facades\Image;
+
+if (!function_exists('formatTime')) {
+    function formatTime()
     {
-        return 'demo';
+        return '1';
+    }
+}
+
+if (!function_exists('uploadImage')) {
+    function uploadImage($file, $authorId)
+    {
+        $pathName = 'public/art/' . $authorId;
+        $path = $file->store($pathName);
+        return $path;
+    }
+}
+
+if (!function_exists('getImageSize')) {
+    function getImageSize($path)
+    {
+        $image = Image::make(storage_path('app/' . $path));
+        $width = $image->width();
+        $height = $image->height();
+        return [
+            'width' => $width,
+            'height' => $height,
+        ];
     }
 }
