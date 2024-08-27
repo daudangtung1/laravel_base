5005754cb3238cef4ccdbe4c3ec6f8ff74e11ea9
daudangtung1 <dangtung.dau.bka@gmail.com>
2024-08-27 09:22:00 +0700

    feature/event-listener: demo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,5 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/{any}', function () {
 //     return view('app');
 // })->where('any', '.*');
+
+Route::get('register-mail', [AuthController::class, 'register']);
