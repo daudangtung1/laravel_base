efbc0f45fedcf34fded866db6c478581f8abe73b
daudangtung1 <dangtung.dau.bka@gmail.com>
2024-08-29 16:35:35 +0700

    feature/front: scss

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Art-app:404</title>
+    <link rel="stylesheet" href="{{ mix('/css/errors.css') }}">
+</head>
+
+<body>
+    <div class="data">
+        123456
+        <div class="data-detail">abcdef</div>
+        <div class="error">xxx</div>
+    </div>
+</body>
+
+</html>
